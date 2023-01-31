554901b39de750ce1b3ce92733dd2ab7f8cc9c33
imranhub10 <alimrancse100@gmail.com>
2023-01-31 19:02:28 +0600

    New Update

--- class2/ForeachLoop.php
+++
@@ -1,8 +0,0 @@
-<?php
-    $city = ["Dhaka", "Tangail", "Gazipur"];
-    echo "<ul>";
-    foreach($city as $value){
-        echo "<li>$value</li>";
-    }
-    echo "</ul>";
-?>
--- class2/OddNumber.php
+++
@@ -1,8 +0,0 @@
-<?php
-//Just Printed Odd Number//
-    for($i = 8; $i <=100; $i++){
-        if($i%2 !=0){
-            echo $i. "<br>";
-        }
-    }
-?>
